63a73151495236054f69a7e370471dadedfcc330
Ashanti01st <shantiwrites4u@gmail.com>
2024-11-14 13:13:13 +0000

    Edits

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from json_functions import read_json
+from start_menu import start_menu
 
 def main():
     people = read_json()
+    start_menu() # Create the start menu in the console
 
 
 if __name__ == "__main__":
---
+++ start_menu.py
@@ -0,0 +1,12 @@
+def start_menu():
+    family_tree = Family_Tree()
+
+    while True:
+      print("||***Welcome to the Emmershon Family Tree***||")
+      print("||***Select any option (1-10)below to learn more about each family member***||")
+      print("1 : Display the entire Family Tree")
+      print("2 : Display parents of a particular family member" )
+      print("3 : Display the grandchildren of a particular family member")
+      print("4 : Display Immediate family members")
+
+#i'm working on this
