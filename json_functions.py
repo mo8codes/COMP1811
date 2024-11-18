f59fc599e9e51247bddd1e70f02126ced1c472cb
mo8 <mo8codes@icloud.com>
2024-11-18 11:59:30 +0000

    Cleanup

--- json_functions.py
+++ json_functions.py
@@ -7,22 +7,20 @@ def create_objects_from_json(objects_db):
         print(person) #Person is a dictionary for example: {'uid': 1, "type": Leaf, 'name': 'John Doe', 'is_male': True, 'is_alive': True, 'date_of_birth': '15-03-1985', 'date_of_death': None, 'spouse': 2, 'parents': {'father': 3, 'mother': 4}}
         #Create the objects from the JSON
         if person["type"] == "Leaf":
-            Leaf(
-
-            )
+            pass#Leaf()
         if person["type"] == "Parent":
             pass#Parent()
     pass
 
-#JSON read file "people.json and betty family"
+#JSON read file "paternal.json and betty family"
 def read_json(db, db2):
-    with open("people.json", "r") as db, open("betty_family.json", "r") as db2:
+    with open("paternal.json", "r") as db, open("maternal.json", "r") as db2:
         objects_db = json.load(db)
         betty_family = json.load(db2)
 
     return create_objects_from_json(objects_db & betty_family)
-db1 = "people.json"
-db2 = "betty_family"
+db1 = "paternal.json"
+db2 = "maternal.json"
 
 print("Data from:", db2)
 
