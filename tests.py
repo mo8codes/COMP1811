eeb1d49c6f8c11a4fb3edde2b4e4ce5ccb59c15f
mo8 <mo8codes@icloud.com>
2024-11-25 20:29:18 +0000

    test classes

--- tests.py
+++ tests.py
@@ -1,12 +1,95 @@
 import unittest
-#from sndhdr import tests
 # https://docs.python.org/3/tutorial/errors.html
 
-# Feature 3aii
+class F1aiTest(unittest.TestCase):
+    """Test for Feature 1a-i: """
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # This is an example
+    def test_name(self):
+        # Implement test to check
+        pass
+class F1aiiTest(unittest.TestCase):
+    """Test for Feature 1a-ii: Immediate Family"""
+
+    def test2_name(self):
+        # Implement test to check
+        pass
+
+class F1biTest(unittest.TestCase):
+    """Test for Feature 1b-i: Immediate Family"""
+
+    def test_immediate_family(self):
+        # Implement test to check immediate family retrieval
+        pass
+
+class F1biiTest(unittest.TestCase):
+    """Test for Feature 1b-ii: Extended Family"""
+
+    def test_extended_family(self):
+        # Implement test to check extended family retrieval
+        pass
+
+class F2aiTest(unittest.TestCase):
+    """Test for Feature 2a-i: Siblings"""
+
+    def test_siblings(self):
+        # Implement test to check sibling retrieval
+        pass
+
+class F2aiiTest(unittest.TestCase):
+    """Test for Feature 2a-ii: Cousins"""
+
+    def test_cousins(self):
+        # Implement test to check cousin retrieval
+        pass
+
+class F2biTest(unittest.TestCase):
+    """Test for Feature 2b-i: Birthdays"""
+
+    def test_birthdays(self):
+        # Implement test to check birthdays list creation
+        pass
+
+class F2biiTest(unittest.TestCase):
+    """Test for Feature 2b-ii: Birthday Calendar"""
+
+    def test_birthday_calendar(self):
+        # Implement test to check sorted birthday calendar
+        pass
+
+class F3aiTest(unittest.TestCase):
+    """Test for Feature 3a-i: Combined Family Program"""
+
+    def test_combined_program(self):
+        # Implement test for integrated program functionality
+        pass
+
+class F3aiiTest(unittest.TestCase):
+    """Test for Feature 3a-ii: Test Features from Partner's Branch"""
+
+    def test_partner_features(self):
+        # Implement test to verify partner branch features
+        pass
+
+class F3aiiiTest(unittest.TestCase):
+    """Test for Feature 3a-iii: Average Age of Death"""
+
+    def test_average_age_of_death(self):
+        # Implement test to calculate average age of death
+        pass
+
+class F3biTest(unittest.TestCase):
+    """Test for Feature 3b-i: Number of Children"""
+
+    def test_number_of_children(self):
+        # Implement test to find number of children
+        pass
+
+class F3biiTest(unittest.TestCase):
+    """Test for Feature 3b-ii: Average Number of Children"""
+
+    def test_average_number_of_children(self):
+        # Implement test to calculate average number of children
+        pass
 
 
 if __name__ == '__main__':
